0345c42988479ef9778ea2e89b5a960f932017cf
Riyaaaaa <s113111@edu.asahikawa-nct.ac.jp>
2016-08-15 17:28:50 +0900

    [修正] Randomの無駄なローカル変数を削除

--- Random/Random.hpp
+++ Random/Random.hpp
@@ -21,16 +21,12 @@ class Random {
 public:
     template<typename T, typename std::enable_if<std::is_integral<T>{}>::type*& = enabler>
     static T getRandom(Range<T> range) {
-        static const std::random_device seed_gen;
-        static ENGINE mt;
         std::uniform_int_distribution<T> dist(range.min, range.max);
         return dist(engine);
     }
     
     template<typename T, typename std::enable_if<std::is_floating_point<T>{}>::type*& = enabler>
     static T getRandom(Range<T> range) {
-        static const std::random_device seed_gen;
-        static ENGINE mt;
         std::uniform_real_distribution<T> dist(range.min, range.max);
         return dist(engine);
     }
