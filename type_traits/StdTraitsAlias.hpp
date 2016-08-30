0059d77fd351246c1adfbe6e3d5b2defa2bcee4d
Riyaaaaa <s113111@edu.asahikawa-nct.ac.jp>
2016-08-30 19:46:22 +0900

    [Bugfix] gcc 4.9, clang 3.2 以前のコンパイラが一部のtype_traitsに対応してない問題

--- type_traits/StdTraitsAlias.hpp
+++ type_traits/StdTraitsAlias.hpp
@@ -40,34 +40,67 @@ TYPE_ALIAS(underlying_type);
 TYPE_ALIAS(result_of);
 
 namespace detail {
+#if __GNUC__ >= 5 || (__clang_major__ >= 5 && __clang_minor__ >= 0)
+    template <class _Tp>
+    struct align_type
+    {
+        static const size_t value = std::alignment_of<_Tp>::value;
+        typedef _Tp type;
+    };
+    
+    template <class _Hp, class _Tp>
+    struct type_list
+    {
+        typedef _Hp _Head;
+        typedef _Tp _Tail;
+    };
+    
+    struct nat
+    {
+        nat() = delete;
+        nat(const nat&) = delete;
+        nat& operator=(const nat&) = delete;
+        ~nat() = delete;
+    };
+
+    struct __struct_double {long double __lx;};
+    struct __struct_double4 {double __lx[4];};
 
 typedef
-std::__type_list<std::__align_type<unsigned char>,
-std::__type_list<std::__align_type<unsigned short>,
-std::__type_list<std::__align_type<unsigned int>,
-std::__type_list<std::__align_type<unsigned long>,
-std::__type_list<std::__align_type<unsigned long long>,
-std::__type_list<std::__align_type<double>,
-std::__type_list<std::__align_type<long double>,
-std::__type_list<std::__align_type<std::__struct_double>,
-std::__type_list<std::__align_type<std::__struct_double4>,
-std::__type_list<std::__align_type<int*>,
-std::__nat
+type_list<align_type<unsigned char>,
+type_list<align_type<unsigned short>,
+type_list<align_type<unsigned int>,
+type_list<align_type<unsigned long>,
+type_list<align_type<unsigned long long>,
+type_list<align_type<double>,
+type_list<align_type<long double>,
+type_list<align_type<__struct_double>,
+type_list<align_type<__struct_double4>,
+type_list<align_type<int*>,
+nat
 > > > > > > > > > > __all_types;
 
+    
+    template <class _TL, size_t _Len> struct __find_max_align;
+    
+    template <class _Hp, size_t _Len>
+    struct __find_max_align<type_list<_Hp, nat>, _Len> : public std::integral_constant<size_t, _Hp::value> {};
+#endif
 }
 
-template <std::size_t Len, std::size_t Align = std::__find_max_align<detail::__all_types, Len>::value>
+#if __GNUC__ >= 5 || (__clang_major__ >= 5 && __clang_minor__ >= 0)
+template <std::size_t Len, std::size_t Align = detail::__find_max_align<detail::__all_types, Len>::value>
 using aligned_storage_t = std::aligned_storage<Len, Align>;
 
 template <std::size_t Len, class... Types>
-using aligned_union_t = typename std::aligned_union<Len,Types...>::type;
+using aligned_union_t = typename std::aligned_union<Len, Types...>::type;
+#endif
 
 template <bool Condition, class T = void>
-using enable_if_t = typename std::enable_if<Condition,T>::type;
+using enable_if_t = typename std::enable_if<Condition, T>::type;
 
 template <bool B, class T, class F>
-using conditional_t = typename std::conditional<B,T,F>::type;
+using conditional_t = typename std::conditional<B, T, F>::type;
 
 template <class... T>
 using common_type_t = typename std::common_type<T...>::type;
@@ -99,7 +132,6 @@ struct alias<T, void> {
     VALUE_ALIAS(is_const);
     VALUE_ALIAS(is_volatile);
     VALUE_ALIAS(is_trivial);
-    VALUE_ALIAS(is_trivially_copyable);
     VALUE_ALIAS(is_standard_layout);
     VALUE_ALIAS(is_pod);
     VALUE_ALIAS(is_literal_type);
@@ -113,6 +145,8 @@ struct alias<T, void> {
     VALUE_ALIAS(is_copy_constructible);
     VALUE_ALIAS(is_move_constructible);
     VALUE_ALIAS(is_destructible);
+#if __GNUC__ >= 5 || (__clang_major__ >= 5 && __clang_minor__ >= 0)
+    VALUE_ALIAS(is_trivially_copyable);
     VALUE_ALIAS(is_trivially_constructible);
     VALUE_ALIAS(is_trivially_default_constructible);
     VALUE_ALIAS(is_trivially_copy_constructible);
@@ -120,6 +154,7 @@ struct alias<T, void> {
     VALUE_ALIAS(is_trivially_destructible);
     VALUE_ALIAS(is_trivially_copy_assignable);
     VALUE_ALIAS(is_trivially_move_assignable);
+#endif
     VALUE_ALIAS(is_nothrow_constructible);
     VALUE_ALIAS(is_nothrow_default_constructible);
     VALUE_ALIAS(is_nothrow_copy_constructible);
@@ -135,7 +170,9 @@ struct alias<T, void> {
 template<class T, class U>
 struct alias {
     VALUE_ALIAS2(is_assignable);
+#if __GNUC__ >= 5 || (__clang_major__ >= 5 && __clang_minor__ >= 0)
     VALUE_ALIAS2(is_trivially_assignable);
+#endif
     VALUE_ALIAS2(is_nothrow_assignable);
     VALUE_ALIAS2(is_same);
     VALUE_ALIAS2(is_base_of);
