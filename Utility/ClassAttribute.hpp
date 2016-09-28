1286ddc867fb936dc23743ddcb1208ef7fc42076
Riyaaaaa <s113111@edu.asahikawa-nct.ac.jp>
2016-09-28 18:44:09 +0900

    Noncopyableクラス追加

---
+++ Utility/ClassAttribute.hpp
@@ -0,0 +1,30 @@
+/*=============================================================================
+ Copyright (c) 2016 Riyaaaaa
+ https://github.com/Riyaaaaa/SpiralLibrary
+ Distributed under the Boost Software License, Version 1.0. (See accompanying
+ file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ =============================================================================*/
+
+#ifndef ClassAttribute_h
+#define ClassAttribute_h
+
+#include "../Common/Macro.h"
+
+NS_LIBSPIRAL_BEGIN
+
+namespace attribute {
+    
+    class Noncopyable {
+    protected:
+        SPIRAL_CONSTEXPR Noncopyable() = default;
+        ~Noncopyable() = default;
+        
+        Noncopyable(const Noncopyable&) = delete;
+        Noncopyable& operator=(const Noncopyable&) = delete;
+    };
+    
+}
+
+NS_LIBSPIRAL_END
+
+#endif /* ClassAttribute_h */
